c776ee150d743c9002330d507393a5c17da895b2
ArPimentel <pimentel.arturo@gmail.com>
2023-06-14 16:57:53 +0200

    created about bottom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DemoPageComponent } from './pages/demo/demo.component';
 
-const routes: Routes = [{ path: '', component: DemoPageComponent }];
+const routes: Routes = [{ }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
