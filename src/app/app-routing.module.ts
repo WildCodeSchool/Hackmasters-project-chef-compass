3d8856002a876baf5774bb8f5e32335058a6d100
remyrm <remy.marquis62121@gmail.com>
2023-06-15 18:56:29 +0200

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DemoPageComponent } from './pages/demo/demo.component';
 
 const routes: Routes = 
 [
   {path: 'about', component: AboutComponent},
-  { path: '', component: HomepageComponent } 
+  { path: '', component: HomepageComponent }, 
+  { path: '', component: DemoPageComponent }
   ];
 
 @NgModule({
@@ -14,3 +16,5 @@ const routes: Routes =
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+
+
