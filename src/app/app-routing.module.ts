a60822d45c91b88e1823364018906df72e832078
td6904 <td665pro@pm.me>
2023-06-15 11:41:23 +0200

    Homepage routing fixed, logo now links to homepage and search bar temporarily commented out, waiting for implemntation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { DemoPageComponent } from './pages/demo/demo.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  // { path: '', component: DemoPageComponent 
+  { path: '', component: HomepageComponent } 
   ];
 
 @NgModule({
