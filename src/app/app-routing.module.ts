9b932491aa5282f903ca0ac12734e9c4511b0ace
ArPimentel <pimentel.arturo@gmail.com>
2023-06-16 15:13:33 +0200

    Resolved conflits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-=======
-
-import { HomepageComponent } from './homepage/homepage.component';
-import { AboutComponent } from './about/about.component';
-import { CommunityComponent } from './community/community.component';
->>>>>>> fcdad5e548750008ad55aa0ec3af0e2f3a3c4915
 import { AllrecipesComponent } from './allrecipes/allrecipes.component';
 
 const routes: Routes = [{ path: 'recipes', component: AllrecipesComponent, pathMatch: 'full' }];
