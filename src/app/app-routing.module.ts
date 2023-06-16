f114b55fc26e1c1e5df728b4174a1503b2505eb1
remyrm <remy.marquis62121@gmail.com>
2023-06-16 12:27:46 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { DemoPageComponent } from './pages/demo/demo.component';
 
 const routes: Routes = 
 [
+
+  
   {path: 'about', component: AboutComponent},
   { path: '', component: HomepageComponent }, 
   { path: '', component: DemoPageComponent }
