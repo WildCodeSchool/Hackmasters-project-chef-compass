172cd788391d5558aafd67ea5766657a029d6201
td6904 <td665pro@pm.me>
2023-06-19 11:13:08 +0200

    Env files tested succesfully - dist file created for other uses

---
+++ src/environment/dist.environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    //Set production environment to true or false based on environment needed.
+    //Don't forget to name the file accordingly i.e:
+    //environment.ts or environment.prod.ts
+    //These files will be then automatically gitignored once named correctly*
+    
+    production: false,
+    apiKey: ''
+}
+
