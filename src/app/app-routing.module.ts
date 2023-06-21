888946f66258f069324da6be01281a3a450d57a6
td6904 <td665pro@pm.me>
2023-06-21 11:31:49 +0200

    responsiveDesign branch created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { SingleRecipeComponent } from './pages/single-recipe/single-recipe.component';;
+import { SingleRecipeComponent } from './pages/single-recipe/single-recipe.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AllRecipesComponent } from './pages/all-recipes/all-recipes.component';
 
-const routes: Routes = 
-[
-  { path: '', component: HomepageComponent }, 
-  { path: 'recipes', component: AllRecipesComponent},
-  { path: 'recipes/:name', component: SingleRecipeComponent},
-  { path: 'about', component: AboutComponent}
-  ];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'recipes', component: AllRecipesComponent },
+  { path: 'recipes/:name', component: SingleRecipeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
-  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
