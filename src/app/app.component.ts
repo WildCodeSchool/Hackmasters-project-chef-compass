fe20fe47f55cc0a94b8d658aa0e5a5db72f437bf
td6904 <td665pro@pm.me>
2023-06-22 14:23:57 +0200

    Modified app template to allow navbar position change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  isNavbarAbove: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event){
+    this.isNavbarAbove = window.innerWidth > 768;
+  }
+}
