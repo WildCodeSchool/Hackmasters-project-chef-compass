2b0ccbfd7f97b368782e6b33c144e502e3c34613
Rémy Marquis <114175213+remy-rm@users.noreply.github.com>
2023-07-12 00:59:18 +0200

    Update scss add animation and fix footer

--- src/app/component/recipes-display/recipes-display.component.ts
+++ src/app/component/recipes-display/recipes-display.component.ts
@@ -11,51 +11,78 @@ import { Recipes } from 'src/app/models/recipes.model';
   templateUrl: './recipes-display.component.html',
   styleUrls: ['./recipes-display.component.scss']
 })
-export class RecipesdisplayComponent implements OnInit{
+export class RecipesdisplayComponent implements OnInit {
   @Input() recipes!: Recipes;
   haveFavorites = this.userService.favoriteRecipes.length > 0;
 
 
-  translateValueDesserts = 0;
-  translateValueMainDishes = 0;
-  translateValueAppetizers = 0;
-  translateValueBreakfasts = 0;
-  translateValueSideDishes = 0;
-  currentRoute=  this.route.snapshot.routeConfig!.path;
+  translateValueDesserts = -275;
+  translateValueMainDishes = -275;
+  translateValueAppetizers = -275;
+  translateValueBreakfasts = -275;
+  translateValueSideDishes = -275;
+  currentRoute = this.route.snapshot.routeConfig!.path;
 
   constructor(private http: HttpClient,
-              private recipesService :RecipesService,
-              private route :ActivatedRoute,
-              private userService: UsersService ) { }
+              private recipesService: RecipesService,
+              private route: ActivatedRoute,
+              private userService: UsersService) {
+  }
+  isPageLoaded = false;
 
-  ngOnInit(): void { }
-              moveLeft(recipe: string) {
-                if (recipe === 'desserts') {
-                  this.translateValueDesserts += 300;
-                } else if (recipe === 'mainDishes') {
-                  this.translateValueMainDishes += 300;
-                } else if (recipe === 'appetizers') {
-                  this.translateValueAppetizers += 300;
-                } else if (recipe === 'breakfasts') {
-                  this.translateValueBreakfasts += 300;
-                } else if (recipe === 'sideDishes') {
-                  this.translateValueSideDishes += 200;
-                }
-              }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isPageLoaded = true;
+    },2000);
+  }
 
-              moveRight(recipe: string) {
-                if (recipe === 'desserts') {
-                  this.translateValueDesserts -= 300;
-                } else if (recipe === 'mainDishes') {
-                  this.translateValueMainDishes -= 300;
-                } else if (recipe === 'appetizers') {
-                  this.translateValueAppetizers -= 300;
-                } else if (recipe === 'breakfasts') {
-                  this.translateValueBreakfasts -= 300;
-                } else if (recipe === 'sideDishes') {
-                  this.translateValueSideDishes -= 300;
-                }
-              }
+  async move(recipe: string, direction: number) {
+    let translateValue = 0;
+    let recipeList;
 
+    // Mapping the recipe categories to their respective variables
+    if (recipe === 'desserts') {
+      recipeList = this.recipes.desserts;
+    } else if (recipe === 'mainDishes') {
+      recipeList = this.recipes.mainDishes;
+    } else if (recipe === 'appetizers') {
+      recipeList = this.recipes.appetizers;
+    } else if (recipe === 'breakfasts') {
+      recipeList = this.recipes.breakfasts;
+    } else if (recipe === 'sideDishes') {
+      recipeList = this.recipes.sideDishes;
+    } else {
+      console.error("Invalid recipe category");
+      return;
+    }
+
+    if (direction > 0) {
+      const lastCard = recipeList[recipeList.length - 1];
+      recipeList.pop();
+      this.changeValue(recipe, -275);
+      recipeList.unshift(lastCard);
+    } else { // Scroll left
+      const firstCard = recipeList[0];
+      recipeList.shift();
+      this.changeValue(recipe, 275);
+      recipeList.push(firstCard);
+    }
+
+  }
+  changeValue(recipe:String,translateValue:number){
+    if (recipe === 'desserts') {
+      this.translateValueDesserts += translateValue;
+    } else if (recipe === 'mainDishes') {
+      this.translateValueMainDishes += translateValue;
+    } else if (recipe === 'appetizers') {
+      this.translateValueAppetizers += translateValue;
+    } else if (recipe === 'breakfasts') {
+      this.translateValueBreakfasts += translateValue;
+    } else if (recipe === 'sideDishes') {
+      this.translateValueSideDishes += translateValue;
+    }
+
+  }
 
 }
+
