021d2f6a7af13d056f2f6fdf6c7e1b68ac4cbb4c
Rémy Marquis <114175213+remy-rm@users.noreply.github.com>
2023-07-25 11:20:08 +0200

    change div for ul and li

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -4,7 +4,7 @@ import { forkJoin, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../../models/modelRecipe/Country.model';
 import { Diet } from '../../models/modelRecipe/Diet.model';
-//import { Users } from 'src/app/models/modelRecipe/Users.model';
+
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +12,12 @@ import { Diet } from '../../models/modelRecipe/Diet.model';
 export class SearchService {
   constructor(private http: HttpClient) {}
 
-  searchUrl = 'http://localhost:8080/recipes/';
+  searchUrl = 'http://localhost:8080';
 
   getMultipleSearch(): Observable<any[]> {
     const country = this.http.get<Country[]>(`${this.searchUrl}/countries`);
     const allergens = this.http.get<Allergen[]>(`${this.searchUrl}/allergens`);
     const diets = this.http.get<Diet[]>(`${this.searchUrl}/diets`);
-    //const firstname = this.http.get<Users[]>(`${this.searchUrl}/users`);
 
     return forkJoin([country, allergens, diets]);
   }
