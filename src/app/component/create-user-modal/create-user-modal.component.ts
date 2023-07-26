56c02e258c588668d2cb093c7740f2a1753db8c1
Rémy Marquis <114175213+remy-rm@users.noreply.github.com>
2023-07-26 10:14:16 +0200

    remove dead code and translate in english

--- src/app/component/create-user-modal/create-user-modal.component.ts
+++ src/app/component/create-user-modal/create-user-modal.component.ts
@@ -44,21 +44,17 @@ export class CreateUserModalComponent implements OnInit {
   }
 
   someFunction() {
-    // Définir la propriété showRegisterForm sur true pour afficher la deuxième fenêtre modale
     this.showRegisterForm = true;
   }
 
-  // Définition de la méthode pour soumettre le formulaire d'enregistrement
   onRegisterSubmit(): void {
     const { firstname, email, password } = this.userForm;
 
-    // Check if the passwords match
     if (password !== this.userForm.confirmPassword) {
       this.errorMessage = 'Passwords do not match.';
       return;
     }
 
-    // Call the service to create a new user
     this.authUserService.registerUser(email, password, firstname).subscribe(
       (response: any) => {
         console.log('New user created successfully!', response);
@@ -85,26 +81,21 @@ export class CreateUserModalComponent implements OnInit {
   }
 
   onResetPasswordSubmit(): void {
-    // Vérifiez si l'e-mail de réinitialisation est valide (vous pouvez ajouter des validations supplémentaires ici)
     if (!this.isValidEmail(this.userForm.resetPasswordEmail)) {
-      this.resetPasswordErrorMessage = 'Veuillez entrer une adresse e-mail valide.';
+      this.resetPasswordErrorMessage = 'invalid email';
       return;
     }
 
-    // Appelez votre service pour gérer la réinitialisation du mot de passe ici
     this.authUserService.resetPassword(this.userForm.resetPasswordEmail).subscribe(
       (response: any) => {
-        // Le mot de passe a été réinitialisé avec succès
-        console.log('Mot de passe réinitialisé avec succès !', response);
+        console.log('password rest', response);
         setTimeout(() => {
           this.resetPasswordErrorMessage = '';
           this.resetPasswordSuccess = true;
         }, 2000);
       },
       (error: any) => {
-        // Gérez ici les erreurs éventuelles lors de la réinitialisation du mot de passe
-        console.error('Erreur lors de la réinitialisation du mot de passe :', error);
-        this.resetPasswordErrorMessage = 'Une erreur est survenue lors de la réinitialisation du mot de passe';
+        this.resetPasswordErrorMessage = 'error while resetting password';
       }
     );
   }
